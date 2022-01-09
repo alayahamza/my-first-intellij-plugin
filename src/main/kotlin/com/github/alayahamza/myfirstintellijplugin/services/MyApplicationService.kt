dad0bdd6391bb85635dd8e5e83f81757d50d194b
GitHub Action <action@github.com>
2022-01-09 18:39:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/alayahamza/myfirstintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alayahamza.myfirstintellijplugin.services
+
+import com.github.alayahamza.myfirstintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
